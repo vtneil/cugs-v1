9a507ddd82dab02ca189240ddd9a9e034fb8af01
Vivatsathorn Thitasirivit <69193033+neil4884@users.noreply.github.com>
2022-04-08 19:51:39 +0700

    - Updated lib_preferences_reader to be able to read JSON, TOML, XML, and YAML format.
    - Remove 'crab' format from lib_preferences_reader.

--- data_handler/lib_preferences_reader.py
+++ data_handler/lib_preferences_reader.py
@@ -1,66 +1,55 @@
-import _io
+import json as _json
+import toml as _toml
+import yaml as _yaml
+import strictyaml as _strictyaml
+import xmltodict as _xmldict
+import dict2xml as _dictxml
+import xml as _xml
 from typing import Union as _Union
-from typing import IO as _IO
+from collections import OrderedDict as _Dict
+from io import StringIO as _String
 
 
 class PreferencesData:
-    def __init__(self, pref: _Union[str, dict, _IO], format_: str='crab') -> None:
+    def __init__(self, pref: _Union[str, dict], /, *, mode: str = 'file') -> None:
         '''
         A class to read custom preference data
 
         :param _pref_filename: Preference file path or name
         '''
-        import json
         self.__pref = pref
-        self.__format = format_
+        self.__mode = mode
+        self.__format = ''
         self.__pref_dict = dict()
-        self.__format_error = KeyError('Got {} as format but only accepts crab and json format.'
+        self.__format_error = KeyError('Got {} as format but only accepts json, yaml, xml and toml formats.'
                                        .format(self.__format))
-        self.__pref_error = TypeError('Got {} as pref but only accepts Union[str, dict, typing.IO].'
+        self.__pref_error = TypeError('Got {} as pref but only accepts Union[str, dict].'
                                       .format(type(self.__pref)))
         if isinstance(self.__pref, str):
-            if self.__format in ['crab', 'json']:
-                try:
-                    with open(self.__pref, mode='r') as _f:
-                        self.__pref_dict = json.load(_f)
-                except ValueError:
-                    with open(self.__pref, mode='r') as _f:
-                        dict_key = ''
-                        for _line in _f:
-                            _line_data = _line.strip()
-                            if not _line_data:
-                                continue
-                            elif _line_data[0] in {'#'}:
-                                continue
-                            elif _line_data[0] in {'['}:
-                                idx_endl = _line_data.rfind(']')
-                                dict_key = _line_data[1:idx_endl]
-                                self.__pref_dict[dict_key] = list()
-                            else:
-                                self.__pref_dict[dict_key].append(_line_data)
-            else:
+            try:
+                self.__pref_dict = self.parseJSON(self.__pref, mode=self.__mode)
+                self.__format = 'json'
+            except ValueError or _json.decoder.JSONDecodeError:
+                pass
+
+            try:
+                self.__pref_dict = self.parseTOML(self.__pref, mode=self.__mode)
+                self.__format = 'toml'
+            except ValueError or _toml.decoder.TomlDecodeError:
+                pass
+
+            try:
+                self.__pref_dict = self.parseXML(self.__pref, mode=self.__mode)
+                self.__format = 'xml'
+            except ValueError or _xml.parsers.expat.ExpatError:
+                pass
+
+            try:
+                self.__pref_dict = self.parseYAML(self.__pref, mode=self.__mode)
+                self.__format = 'yaml'
+            except Exception:
                 raise self.__format_error
-        elif isinstance(self.__pref, _IO):
-            if self.__format == 'crab':
-                with self.__pref as _f:
-                    dict_key = ''
-                    for _line in _f:
-                        _line_data = _line.strip()
-                        if not _line_data:
-                            continue
-                        elif _line_data[0] in {'#'}:
-                            continue
-                        elif _line_data[0] in {'['}:
-                            idx_endl = _line_data.rfind(']')
-                            dict_key = _line_data[1:idx_endl]
-                            self.__pref_dict[dict_key] = list()
-                        else:
-                            self.__pref_dict[dict_key].append(_line_data)
-            elif self.__format == 'json':
-                with self.__pref as _f:
-                    self.__pref_dict = json.load(_f)
-            else:
-                raise self.__format_error
+
         elif isinstance(self.__pref, dict):
             self.__pref_dict = self.__pref
         else:
@@ -74,7 +63,7 @@ class PreferencesData:
         '''
         return self.__pref_dict
 
-    def writePreferences(self, file_format = None) -> bool:
+    def writePreferences(self, file_format=None) -> bool:
         '''
         Write preferences to file.
         Internal Python functions only update the preferences dictionary, but
@@ -82,51 +71,15 @@ class PreferencesData:
 
         :return: True if successful, False if failed
         '''
-        __ff = file_format
-        if not __ff:
-            __ff = self.__format
-        if isinstance(self.__pref, str):
-            if __ff == 'crab':
-                try:
-                    with open(self.__pref, mode='w') as _f:
-                        for line in self.__generateCrabFileFormat(self.__pref_dict):
-                            _f.write(line)
-                            _f.write('\n')
-                    return True
-                except Exception:
-                    return False
-            elif __ff == 'json':
-                import json
-                try:
-                    __json_obj = json.dumps(self.__pref_dict, indent=4)
-                    with open(self.__pref, mode='w') as _f:
-                        _f.write(__json_obj)
-                    return True
-                except Exception:
-                    return False
-        elif isinstance(self.__pref, _IO):
-            if __ff == 'crab':
-                try:
-                    with self.__pref as _f:
-                        for line in self.__generateCrabFileFormat(self.__pref_dict):
-                            _f.write(line)
-                            _f.write('\n')
-                    return True
-                except Exception:
-                    return False
-            elif __ff == 'json':
-                import json
-                try:
-                    __json_obj = json.dumps(self.__pref_dict, indent=4)
-                    with self.__pref as _f:
-                        _f.write(__json_obj)
-                    return True
-                except Exception:
-                    return False
-        elif isinstance(self.__pref, dict):
-            return False
+        if file_format is None:
+            __file_format = self.__format
         else:
-            raise self.__format_error
+            __file_format = file_format
+
+        if __file_format == 'json':
+            pass
+
+        return False
 
     def setdefault(self, key, value):
         self.__pref_dict.setdefault(key, value)
@@ -169,15 +122,111 @@ class PreferencesData:
         return str(self)
 
     @staticmethod
-    def __generateCrabFileFormat(inp_dict: dict):
-        for k, v in inp_dict.items():
-            yield '[' + str(k) + ']'
-            for elem in v:
-                yield str(elem)
-            yield ''
+    def parseJSON(inp_str: str, /, mode: str = 'str') -> dict:
+        if mode == 'str':
+            return _json.loads(inp_str)
+        elif mode == 'file':
+            with open(inp_str, mode='r', encoding='utf-8') as f:
+                return _json.load(f)
+        else:
+            raise KeyError('mode only supports str and file!')
+
+    @staticmethod
+    def parseTOML(inp_str: str, /, mode: str = 'str') -> dict:
+        if mode == 'str':
+            return _toml.loads(inp_str)
+        elif mode == 'file':
+            with open(inp_str, mode='r', encoding='utf-8') as f:
+                return _toml.load(f)
+        else:
+            raise KeyError('mode only supports str and file!')
+
+    @staticmethod
+    def parseXML(inp_str: str, /, mode: str = 'str', *, disorder: bool = True) -> dict:
+        if mode == 'str':
+            if disorder:
+                return PreferencesData.disorderDict(_xmldict.parse(inp_str, encoding='utf-8'))
+            else:
+                return _xmldict.parse(inp_str, encoding='utf-8')
+        elif mode == 'file':
+            with open(inp_str, mode='r', encoding='utf-8') as f:
+                if disorder:
+                    return PreferencesData.disorderDict(_xmldict.parse(''.join(f.readlines()), encoding='utf-8'))
+                else:
+                    return _xmldict.parse(''.join(f.readlines()), encoding='utf-8')
+
+    @staticmethod
+    def parseYAML(inp_str: str, /, mode: str = 'str', *, strict_validate: bool = False) -> dict:
+        if strict_validate:
+            if mode == 'str':
+                f = _String(inp_str)
+                return _strictyaml.load(f)
+            elif mode == 'file':
+                with open(inp_str, mode='r', encoding='utf-8') as f:
+                    return _strictyaml.load(f)
+        else:
+            if mode == 'str':
+                f = _String(inp_str)
+                return _yaml.safe_load(f)
+            elif mode == 'file':
+                with open(inp_str, mode='r', encoding='utf-8') as f:
+                    return _yaml.safe_load(f)
+            else:
+                raise KeyError('mode only supports str and file!')
+
+    @staticmethod
+    def writeJSON(pref_to_write: dict, inp_file_dir: str, *, _indent: int = 4) -> bool:
+        if pref_to_write:
+            with open(inp_file_dir, mode='w', encoding='utf-8') as f:
+                _json.dump(pref_to_write, f, indent=_indent)
+            return True
+        return False
+
+    @staticmethod
+    def writeTOML(pref_to_write: dict, inp_file_dir: str, *, _indent: int = 4) -> bool:
+        if pref_to_write:
+            with open(inp_file_dir, mode='w', encoding='utf-8') as f:
+                _toml.dump(pref_to_write, f)
+            return True
+        return False
+
+    @staticmethod
+    def writeXML(pref_to_write: dict, inp_file_dir: str, *, _indent: int = 4) -> bool:
+        if pref_to_write:
+            with open(inp_file_dir, mode='w', encoding='utf-8') as f:
+                s = _dictxml.dict2xml(pref_to_write)
+                f.writelines(s)
+            return True
+        return False
+
+    @staticmethod
+    def writeYAML(pref_to_write: dict, inp_file_dir: str, *, _indent: int = 4) -> bool:
+        if pref_to_write:
+            with open(inp_file_dir, mode='w', encoding='utf-8') as f:
+                s = _yaml.dump(pref_to_write)
+                f.writelines(s)
+            return True
+        return False
+
+    @staticmethod
+    def disorderDict(inp_dict: _Union[dict, _Dict]) -> dict:
+        return _json.loads(_json.dumps(inp_dict))
+
 
 if __name__ == '__main__':
     f = open('data_format.txt')
-    with f:
-        for line in f:
-            print(line)
+    f.close()
+    # s = PreferencesData.parseXML('<EmpData><EmpID>12345</EmpID><EmpName>Vivatsathorn</EmpName></EmpData>', mode='str')
+    s = PreferencesData.parseYAML('hello:\n    - image\n    - command', strict_validate=False)
+    ptr = {'x': {'hello': 'image', 'tt': 'command', 'qq': ['jkdashifk', 'dghsdhfj']}}
+    PreferencesData.writeJSON(ptr, 'write_test_no_usage/writejson')
+    print(PreferencesData.parseJSON('write_test_no_usage/writejson', 'file'))
+
+    PreferencesData.writeTOML(ptr, 'write_test_no_usage/writetoml')
+    print(PreferencesData.parseTOML('write_test_no_usage/writetoml', 'file'))
+
+    PreferencesData.writeXML(ptr, 'write_test_no_usage/writexml')
+    print(PreferencesData.parseXML('write_test_no_usage/writexml', 'file'))
+
+    PreferencesData.writeYAML(ptr, 'write_test_no_usage/writeyaml')
+    print(PreferencesData.parseYAML('write_test_no_usage/writeyaml', 'file'))
